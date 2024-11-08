2192bb10ade20f2dab8a94085f9899c94c8f2933
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-08 16:55:56 +0000

    Trip expenditure

---
+++ main.py
@@ -0,0 +1,7 @@
+hotel = float(input("Enter dollars spent on hotel:"))
+cab =float(input("Enter dollars spent on cab:"))
+flight=float(input("Enter dollars spent on flight:"))
+
+totalcost = hotel + cab + flight
+
+print("total is:", totalcost)
